5e1468c284d76b0ea022034181328c4b766717dd
Craig Paul <craig.paul@coconutcalendar.com>
2018-11-15 12:32:56 -0600

    Adds test for property instance

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,5 @@
 import OpenApi from './index';
+import User from './resources/user';
 
 it('will be constructed with appropriate options', async () => {
   const instance = new OpenApi({
@@ -9,4 +10,13 @@ it('will be constructed with appropriate options', async () => {
     domain: 'admin',
     version: 'v2',
   });
+});
+
+it('can access the user resource', async () => {
+  const instance = new OpenApi({
+    domain: 'admin',
+  });
+
+  expect(instance).toHaveProperty('users');
+  expect(instance.users).toBeInstanceOf(User.prototype.constructor);
 });
