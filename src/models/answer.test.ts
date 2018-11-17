59a907da2b3043b6a917e9f9507aa9f14fa1545e
Craig Paul <craig.paul@coconutcalendar.com>
2018-11-17 12:49:29 -0600

    Adjusts answer tests

--- src/models/answer.test.ts
+++ src/models/answer.test.ts
@@ -1,13 +1,10 @@
 import Answer from './answer';
 
-it('will set the question identifier', async () => {
+it('will set the question identifier and answer value', async () => {
   const answer = new Answer;
 
-  expect(answer.for(1)).toHaveProperty('question', 1);
-});
-
-it('will set the answer value', async () => {
-  const answer = new Answer;
-
-  expect(answer.is('the answer')).toHaveProperty('value', 'the answer');
+  expect(answer.for(1).is('the answer')).toHaveProperty('attributes', {
+    question: 1,
+    value: 'the answer',
+  });
 });
