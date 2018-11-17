5102744265861bc4263d9149deb7306dd9efdf80
Craig Paul <craig.paul@coconutcalendar.com>
2018-11-17 14:46:10 -0600

    Adds answers attribute setter

--- src/models/attendee.test.ts
+++ src/models/attendee.test.ts
@@ -1,2 +1,24 @@
+import Answer from './answer';
 import Attendee from './attendee';
 
+it('can set answers when providing a single answer', async () => {
+  const answer = new Answer;
+  const attendee = new Attendee;
+
+  expect(attendee.answers(answer).getAttributes()).toEqual(
+    expect.objectContaining({
+      answers: [answer],
+    })
+  );
+});
+
+it('can set answers when providing multiple answers', async () => {
+  const answer = new Answer;
+  const attendee = new Attendee;
+
+  expect(attendee.answers([answer, answer]).getAttributes()).toEqual(
+    expect.objectContaining({
+      answers: [answer, answer],
+    })
+  );
+});
--- src/models/attendee.ts
+++ src/models/attendee.ts
@@ -1,10 +1,13 @@
 import { AnswerModel, AttendeeModel } from '../types/models';
 import { AttendeeParameters, LocationDetailParameters, ReachableDetailParameters } from '../types/parameters';
+import Model from './model';
 
-export default class Attendee implements AttendeeModel {
+export default class Attendee extends Model implements AttendeeModel {
   protected attributes: AttendeeParameters;
 
   constructor() {
+    super();
+
     this.attributes = {
       email: null,
       first_name: null,
@@ -13,6 +16,8 @@ export default class Attendee implements AttendeeModel {
   }
 
   public answers(answers: AnswerModel | AnswerModel[]): this {
+    this.attributes.answers = Array.isArray(answers) ? answers : [answers];
+
     return this;
   }
 
