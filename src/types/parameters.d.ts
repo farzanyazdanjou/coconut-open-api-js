ab22acab62606f98125b10a2be04163bae073d0d
Craig Paul <craig.paul@coconutcalendar.com>
2018-11-17 14:02:05 -0600

    Adds missing answers parameter. Reorders parameters

--- src/types/parameters.d.ts
+++ src/types/parameters.d.ts
@@ -1,29 +1,32 @@
+import { AnswerModel } from './models';
+
 export interface AnswerParameters {
   question: number | null;
   value: string | null;
 }
 
 export interface AttendeeParameters {
+  address?: string;
+  answers?: AnswerModel | AnswerModel[];
+  cell_phone?: string;
+  city?: string;
+  country?: string;
+  email: string | null;
   first_name: string | null;
   last_name: string | null;
-  email: string | null;
-  address?: string;
-  city?: string;
-  region?: string;
-  country?: string;
-  postcode?: string;
-  timezone?: string;
   phone?: string;
-  cell_phone?: string;
+  postcode?: string;
+  region?: string;
+  timezone?: string;
   work_phone?: string;
 }
 
 export interface LocationDetailParameters {
   address?: string;
   city?: string;
-  region?: string;
   country?: string;
   postcode?: string;
+  region?: string;
   timezone?: string;
 }
 
@@ -45,9 +48,9 @@ export interface QuestionParameters {
 }
 
 export interface ReachableDetailParameters {
+  cell_phone?: string;
   email: string;
   phone?: string;
-  cell_phone?: string;
   work_phone?: string;
 }
 
