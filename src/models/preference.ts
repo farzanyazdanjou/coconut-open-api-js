7bdd3d296706c65d22f27a21c8b3ed566157744a
Craig Paul <craig.paul@coconutcalendar.com>
2018-11-19 14:24:23 -0600

    Converts to template strings

--- src/models/preference.ts
+++ src/models/preference.ts
@@ -8,8 +8,8 @@ const CERTAIN_DAYS = 2;
 export default class Preference extends Model implements PreferenceModel {
   public static now() {
     const today = new Date;
-    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
 
     return `${date} ${time}`;
   }
